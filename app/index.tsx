267c460716e8792eaf8bd3d6c5882916884fc2ad
Aster547 <samuelchristophervisarra@gmail.com>
2025-05-29 18:14:28 +0800

    [Onboard Screen][Samuel] Just made a few changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,22 @@
 import { useRouter } from 'expo-router';
-import { Button, SafeAreaView, Text, View } from 'react-native';
+import { TouchableOpacity, SafeAreaView, Text, View } from 'react-native';
 
 export default function Index() {
   const router = useRouter();
 
   return (
-    <SafeAreaView className="flex-1 justify-center items-center">
-      <View>
-        <Text className='text-3xl font-bold'>
-          THIS IS THE START SCREEN 
+    <SafeAreaView className="flex-1 bg-[#5E418F] justify-center items-center">
+      <View className = 'w-full items-center justify-center'>
+        <Text className='text-3xl font-bold text-white'>
+          dreamify
         </Text>
-        <Button title="Go to mainscreen" onPress={() => router.navigate('/mainscreen')}/>
+        <TouchableOpacity
+          className="w-4/5 mt-4 py-2 bg-white rounded-full items-center justify-center"
+          onPress={() => router.navigate('/mainscreen')}
+        >
+          <Text className="text-black text-2xl font-bold">Get started</Text>
+        </TouchableOpacity>      
       </View>
     </SafeAreaView>
-  );}
+  );
+}
--- app/mainscreen.tsx
+++ app/mainscreen.tsx
@@ -3,7 +3,7 @@ import { SafeAreaView, Text, View } from 'react-native';
 
 export default function TabTwoScreen() {
   return (
-    <SafeAreaView className='flex-1 justify-center items-center'>
+    <SafeAreaView className='bg-white flex-1 justify-center items-center'>
       <View>
         <Text className='text-3xl font-bold'>
           THIS IS MAIN SCREEN 
