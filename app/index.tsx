504303cbf26cfd3443e9573cb262601b350d58a0
Aster547 <samuelchristophervisarra@gmail.com>
2025-05-29 21:44:33 +0800

    [Onboarding Screen][Samuel] Now it has a background

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,35 @@
 import { useRouter } from 'expo-router';
-import { TouchableOpacity, SafeAreaView, Text, View, Image } from 'react-native';
+import { TouchableOpacity, SafeAreaView, Text, Image, ImageBackground } from 'react-native';
 
 export default function Index() {
   const router = useRouter();
 
   return (
-    <SafeAreaView className="flex-1 bg-[#5E418F] justify-center items-center">
-      <Image
-        source={require('../assets/images/dreamify.png')}
-        className="w-full absolute top-32"
-        resizeMode="contain">
-      </Image>  
-      <TouchableOpacity
-        className="w-4/5 mt-4 py-2 bg-white rounded-full items-center justify-center absolute bottom-72"
-        onPress={() => router.navigate('/mainscreen')}
-      >
-        <Text className="text-[#361274] text-2xl font-bold">Get started</Text>
-      </TouchableOpacity> 
-      <Image
-        source={require('../assets/images/moon.png')}
-        className="w-full absolute bottom-0"
-        resizeMode="contain">
-      </Image>  
-    </SafeAreaView>
+    <ImageBackground
+      source={require('../assets/images/background.png')} // Use your local image here
+      style={{ flex: 1, width: '100%', height: '100%' }}
+      resizeMode="cover"
+    >
+      <SafeAreaView className="flex-1 items-center bg-transparent relative">
+        <Image
+          source={require('../assets/images/dreamify.png')}
+          className="w-full mt-8"
+          resizeMode="contain"
+        />
+        <TouchableOpacity
+          className="w-4/5 py-4 mt-44 bg-white rounded-full items-center justify-center"
+          onPress={() => router.push('/mainscreen')}
+          style={{ zIndex: 1 }}
+        >
+          <Text className="text-[#361274] text-2xl font-bold">Get started</Text>
+        </TouchableOpacity>
+        <Image
+          source={require('../assets/images/moon.png')}
+          className="w-full absolute bottom-0"
+          resizeMode="contain"
+          style={{ zIndex: 0 }}
+        />
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
